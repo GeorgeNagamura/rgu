3e66324cd56cc399d72fe4dac39328a5dcd7ba7c
GeorgeNagamura <ssamuca@gmail.com>
2020-06-08 14:48:23 -0300

    Updates

---
+++ precache-manifest.8810da24e7707bd7c3a9008f6863ee16.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9eb5aa3668a2eac91b69c3775990f01f",
+    "url": "/rgu/index.html"
+  },
+  {
+    "revision": "05a08ca87f6182647085",
+    "url": "/rgu/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "da3e15bc1d3cc1713b5b",
+    "url": "/rgu/static/js/2.4a5e215d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/rgu/static/js/2.4a5e215d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "05a08ca87f6182647085",
+    "url": "/rgu/static/js/main.315cd48b.chunk.js"
+  },
+  {
+    "revision": "22b901d12c8c0c0dafec",
+    "url": "/rgu/static/js/runtime-main.55622482.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/rgu/static/media/logo.5d5d9eef.svg"
+  }
+]);
